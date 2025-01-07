c491c884d6597aa52f161805d8fc3626097b6c42
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-01-07 13:03:36 -0300

    criando e modulando toolTip

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
-
-
 import { acordion } from "./modules/accordion.js"
 import { initTab } from "./modules/initTab.js"
 import { scrollSuave, animationScroll } from "./modules/scroll-suave.js"
 import { modal } from "./modules/modal.js"
+import { toolTip } from "./modules/toolTip.js"
+
+
+
+
 initTab()
 acordion()
 scrollSuave()
 animationScroll()
 modal()
+toolTip()
---
+++ js/modules/toolTip.js
@@ -0,0 +1,18 @@
+export function toolTip(){
+
+}
+
+const tooltips = document.querySelectorAll('[data-toltip]')
+
+tooltips.forEach((item ) => {
+    item.addEventListener('mouseover', onMouseOver)
+});
+
+function onMouseOver(e) {
+
+}
+
+
+function criarTooltipBox() {
+    
+}
