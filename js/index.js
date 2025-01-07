ac8378e8b7398934c5a295b228ebfe078ba4b737
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-01-07 12:19:26 -0300

    finalização modal

--- js/index.js
+++ js/index.js
@@ -3,8 +3,9 @@
 import { acordion } from "./modules/accordion.js"
 import { initTab } from "./modules/initTab.js"
 import { scrollSuave, animationScroll } from "./modules/scroll-suave.js"
-
+import { modal } from "./modules/modal.js"
 initTab()
 acordion()
 scrollSuave()
 animationScroll()
+modal()
