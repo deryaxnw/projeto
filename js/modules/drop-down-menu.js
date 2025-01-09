06bca79b0525d20454a8d8f3f439d3d864ca6e44
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-01-09 15:01:29 -0300

    logica de outClick

--- js/modules/drop-down-menu.js
+++ js/modules/drop-down-menu.js
@@ -1,18 +1,28 @@
 export function DropDown() {
+  const DropDownMenu = document.querySelectorAll("[data-dropdown]");
 
-    const DropDownMenu = document.querySelectorAll('[data-dropdown]')
-    
-    DropDownMenu.forEach(menu => {
-        
-        ['touchstart', 'click'].forEach(userEvent => {
-    
-            menu.addEventListener(userEvent, handleClick)
-        })
-    })
-    
-    function handleClick(e){
-        e.preventDefault();
-        this.classList.toggle('active')
+  DropDownMenu.forEach((menu) => {
+    ["touchstart", "click"].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    });
+  });
+
+  function handleClick(e) {
+    e.preventDefault();
+    this.classList.add("active");
+    outsideClick(this, () => {
+      this.classList.remove("active");
+    });
+  }
+
+  function outsideClick(element, callback) {
+    const html = document.documentElement;
+    html.addEventListener("click", handleOutsideClick);
+    function handleOutsideClick(e) {
+      if (!element.contains(e.target)) {
+          callback();
+
+      }
     }
+  }
 }
-
